ad2f9d9fef76333d17ef56d856b9a0129cd95afc
Vincent Lombard <vincent.lombard@gmail.com>
2015-04-30 14:51:30 +0200

    Ajout dockerhost javascript

--- start.sh
+++ start.sh
@@ -18,7 +18,10 @@ if [ ! -z "$TEMPLATES" ]; then
   su $NUXEO_USER -m -c "sed -i'.backup' 's|\(^nuxeo.templates=\).*|\1$TEMPLATES|g' $NUXEO_CONF"
 fi
 
-
 # Start nuxeo
 su $NUXEO_USER -m -c "$NUXEOCTL --quiet start"
+#Where Am I
+curl -s http://172.17.42.1:2375/v1.17/info |tr "," "\n"|grep \"Name|head -1|cut -d ":" -f2
+echo var dockerhost=$(curl -s http://172.17.42.1:2375/v1.17/info |tr "," "\n"|grep \"Name|head -1|cut -d ":" -f2)\; >>/var/lib/nuxeo/server/nxserver/nuxeo.war/scripts/nxtimezone.js
+
 su $NUXEO_USER -m -c "tail -f /var/log/nuxeo/server.log"
